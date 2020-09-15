12f3173756fb2d9f66d1bf94140f88f6a1050931
feature_liwg <liwg@hyjx.com>
2020-09-15 15:02:17 +0800

    修改文件为模块前

---
+++ src/main/java/com/lwg/mango/admin/mapper/SysDictMapper.java
@@ -0,0 +1,30 @@
+package com.lwg.mango.admin.mapper;
+
+import com.lwg.mango.admin.pojo.SysDict;
+import com.lwg.mango.admin.pojo.SysDictExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface SysDictMapper {
+    long countByExample(SysDictExample example);
+
+    int deleteByExample(SysDictExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SysDict record);
+
+    int insertSelective(SysDict record);
+
+    List<SysDict> selectByExample(SysDictExample example);
+
+    SysDict selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") SysDict record, @Param("example") SysDictExample example);
+
+    int updateByExample(@Param("record") SysDict record, @Param("example") SysDictExample example);
+
+    int updateByPrimaryKeySelective(SysDict record);
+
+    int updateByPrimaryKey(SysDict record);
+}
---
+++ src/main/java/com/lwg/mango/admin/mapper/SysRoleMapper.java
@@ -0,0 +1,30 @@
+package com.lwg.mango.admin.mapper;
+
+import com.lwg.mango.admin.pojo.SysRole;
+import com.lwg.mango.admin.pojo.SysRoleExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface SysRoleMapper {
+    long countByExample(SysRoleExample example);
+
+    int deleteByExample(SysRoleExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SysRole record);
+
+    int insertSelective(SysRole record);
+
+    List<SysRole> selectByExample(SysRoleExample example);
+
+    SysRole selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") SysRole record, @Param("example") SysRoleExample example);
+
+    int updateByExample(@Param("record") SysRole record, @Param("example") SysRoleExample example);
+
+    int updateByPrimaryKeySelective(SysRole record);
+
+    int updateByPrimaryKey(SysRole record);
+}
